4ea79cc773ba5084f1fa9d4b5a7f4fc4d6fa6590
Mateusz <1b13ostalecki@gmail.com>
2020-01-03 20:25:52 +0100

    Add jump and fix floor scrolling

--- scenes/game.lua
+++ scenes/game.lua
@@ -18,18 +18,31 @@ local scoreText
 local uiGroup
 
 local function gameLoop()
- 
+	print("-----------------------------------------------")
 	-- Move floor that drifted off screen back to the start
-	if (floor1.x < -408) then
-		floor1.x = 1006
+	if (floor1.x  < -700) then
+		floor1.x = floor2.x + 1400
 	end
 
-	if (floor2.x < -408) then
-		floor2.x = 1006
+	if (floor2.x < -700) then
+		floor2.x = floor1.x + 1400
 	end
 
 
 end
+
+local function onKeyEvent( event )
+	-- Jumping
+	if ( event.keyName == "space" ) then
+        if (event.phase == "down" and math.floor(myRectangle.y) == 595) then
+			myRectangle:setLinearVelocity(0, -450)
+        end
+	end
+	--TODO: crouching/slide
+
+    return false
+end
+
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
 -- -----------------------------------------------------------------------------------
@@ -40,7 +53,7 @@ function scene:create( event )
 	local sceneGroup = self.view
 	-- Code here runs when the scene is first created but has not yet appeared on screen
 	uiGroup = display.newGroup()    -- Display group for UI objects like the score
-	sceneGroup:insert( uiGroup )
+	sceneGroup:insert( uiGroup ) 
 
 end
 
@@ -55,31 +68,33 @@ function scene:show( event )
 		-- Code here runs when the scene is still off screen (but is about to come on screen)
 		physics.pause()
 
-		myRectangle = display.newRect( 60, 50, 50, 50 )
+		myRectangle = display.newRect( 60, 590, 50, 50 )
 		myRectangle.strokeWidth = 3
 		myRectangle:setFillColor( 0.5 )
 		myRectangle:setStrokeColor( 1, 0, 0 )
-		physics.addBody( myRectangle, "dynamic", {60, 50, 110, 50, 160, 100, 60, 100} )
+		physics.addBody( myRectangle, "dynamic", {60, 590, 110, 590, 160, 640, 60, 640} )
+		myRectangle.gravityScale = 4
 
-		floor1 = display.newRect( 0, 300, 700, 50 )
+		floor1 = display.newRect( 700, 650, 1400, 50 )
 		floor1.strokeWidth = 3
 		floor1:setFillColor( 0.5 )
 		floor1:setStrokeColor( 1, 1, 0 )
-		physics.addBody( floor1, "kinematic", { 0,300, 300,300, 300,350, 0,350 } )
-		floor1:setLinearVelocity(-100, 0)
+		physics.addBody( floor1, "kinematic", { 0,650, 1400,650, 1400,700, 0,700 } )
+		floor1:setLinearVelocity(-333, 0)
 		floor1.gravityScale = 0
 
-		floor2 = display.newRect( 710, 300, 700, 50 )
+		floor2 = display.newRect( 2100, 650, 1400, 50 )
 		floor2.strokeWidth = 3
 		floor2:setFillColor( 0.5 )
 		floor2:setStrokeColor( 1, 1, 0 )
-		physics.addBody( floor2, "kinematic", { 710,300, 1010,300, 1010,350, 710,350 } )
-		floor2:setLinearVelocity(-100, 0)
+		physics.addBody( floor2, "kinematic", { 1400,650, 2800,650, 2800,700, 1400,700 } )
+		floor2:setLinearVelocity(-333, 0)
 		floor2.gravityScale = 0
 
 	elseif ( phase == "did" ) then
 		-- Code here runs when the scene is entirely on screen
 		physics.start()
+		Runtime:addEventListener( "key", onKeyEvent )
 		gameLoopTimer = timer.performWithDelay( 100, gameLoop, 0 )
 	end
 end
@@ -96,7 +111,6 @@ function scene:hide( event )
 
 	elseif ( phase == "did" ) then
 		-- Code here runs immediately after the scene goes entirely off screen
-		
 	end
 end
 

a0af123b13e403889dcba6c5120ae8e56afbbfc1
Mateusz <1b13ostalecki@gmail.com>
2020-01-04 23:15:42 +0100

    Add obstacle disposal mechanism and background scrolling

--- scenes/game.lua
+++ scenes/game.lua
@@ -12,6 +12,8 @@ physics.setGravity(0, 6)
 local hero
 local floor1
 local floor2
+local background1
+local background2
 
 local obstacleTable = {}
 
@@ -33,7 +35,6 @@ local function createObstacle()
 end
 
 local function gameLoop()
-	print("-----------------------------------------------")
 	-- Move floor that drifted off screen back to the start
 	if (floor1.x  < -700) then
 		floor1.x = floor2.x + 1400
@@ -43,7 +44,16 @@ local function gameLoop()
 		floor2.x = floor1.x + 1400
 	end
 
-	createObstacle()
+	-- Dispose of obstacles that drifted off screen
+	for i = #obstacleTable, 1, -1 do
+        local thisObstacle = obstacleTable[i]
+ 
+        if ( thisObstacle.x < -100 or thisObstacle.y > 770)
+		then
+            display.remove( thisObstacle )
+            table.remove( obstacleTable, i )
+        end
+    end
 
 end
 
@@ -110,12 +120,17 @@ local sequences_hero = {
 local function heroListener( event )
  
 	local thisSprite = event.target  -- "event.target" references the sprite
-	print(math.round(event.target.y))
-	print(hero.isJumping)
+	--print(math.round(event.target.y))
+	--print(hero.isJumping)
 end
 
 local function swapSprite()
 
+end
+
+local function scrollBackground(background)
+	background.x = 2870
+	transition.to( background, { time=60000, alpha=1, x=-960, y=background.y, onComplete=scrollBackground} )
 end
 
 -- -----------------------------------------------------------------------------------
@@ -142,12 +157,24 @@ function scene:show( event )
 		-- Code here runs when the scene is still off screen (but is about to come on screen)
 		physics.pause()
 
+		background1 = display.newImageRect( "images/background_run.png", 1920, 1080 )
+		background1.x = 960
+		background1.y = display.contentCenterY
+		transition.to( background1, { time=30000, alpha=1, x=-960, y=background1.y, onComplete=scrollBackground} )
+
+		background2 = display.newImageRect( "images/background_run.png", 1920, 1080 )
+		background2.x = background1.x + 1910
+		background2.y = display.contentCenterY
+		transition.to( background2, { time=60000, alpha=1, x=-960, y=background2.y, onComplete=scrollBackground} )
+		
+
 		hero = display.newSprite( sheet_hero, sequences_hero )
 		hero.x = 100
 		hero.y = 524
 		hero.isJumping = false
 		physics.addBody( hero, "dynamic", {60, 590, 110, 590, 160, 640, 60, 640} )
 		hero.gravityScale = 6
+		hero.isFixedRotation = true
 
 		hero:setStrokeColor( 1, 0 ,0 )
 		hero.strokeWidth = 1
@@ -177,6 +204,7 @@ function scene:show( event )
 		hero:setSequence( "normalRun" )
 		hero:play()
 		gameLoopTimer = timer.performWithDelay( 1000, gameLoop, 0 )
+		spawnTimer = timer.performWithDelay(2000, createObstacle, 0)
 	end
 end
 

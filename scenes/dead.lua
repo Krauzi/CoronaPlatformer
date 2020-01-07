68824de97997c6d258405739e0b59484efe0e5b4
Matikones <matiadamek97@gmail.com>
2020-01-07 17:18:51 +0100

    Add score, lives, new scene

---
+++ scenes/dead.lua
@@ -0,0 +1,78 @@
+local composer = require( "composer" )
+
+local scene = composer.newScene()
+--------------------------------------------------------------------------------------
+
+local function gotoMenu()
+	composer.gotoScene( "scenes.menu", { time=400, effect="crossFade" } )
+end
+
+-- -----------------------------------------------------------------------------------
+-- Scene event functions
+-- -----------------------------------------------------------------------------------
+
+-- create()
+function scene:create( event )
+	local sceneGroup = self.view
+	composer.removeScene("scenes.game")
+
+	local background = display.newImageRect( sceneGroup, "/images/background.jpg", 1280, 720 )
+	background.x = display.contentCenterX
+	background.y = display.contentCenterY
+
+	local losetext = display.newText( sceneGroup, "Przegrałeś!", display.contentCenterX, 180, "fonts/Pixellari.ttf", 112 )
+	losetext:setFillColor( 249/255, 111/255, 41/255 )
+
+	local scoretext = display.newText( sceneGroup, "Twój wynik: " .. _G.finalScore, display.contentCenterX, 280, "fonts/Pixellari.ttf", 86 )
+	scoretext:setFillColor( 1, 1, 1)
+	
+	local MenuButton = display.newText( sceneGroup, "Wróć", display.contentCenterX, 400, "fonts/Pixellari.ttf", 64 )
+	MenuButton:setFillColor( 1, 1, 1 )
+	MenuButton:addEventListener( "tap", gotoMenu)
+end
+
+
+-- show()
+function scene:show( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is still off screen (but is about to come on screen)
+	elseif ( phase == "did" ) then
+		-- Code here runs when the scene is entirely on screen	
+	end
+end
+
+-- hide()
+function scene:hide( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is on screen (but is about to go off screen)
+
+	elseif ( phase == "did" ) then
+		composer.removeScene( "scenes.dead" )
+	end
+end
+
+
+-- destroy()
+function scene:destroy( event )
+	local sceneGroup = self.view
+	-- Code here runs prior to the removal of scene's view
+	-- Dispose audio!
+end
+
+
+-- -----------------------------------------------------------------------------------
+-- Scene event function listeners
+-- -----------------------------------------------------------------------------------
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
+
+return scene
--- scenes/game.lua
+++ scenes/game.lua
@@ -29,26 +29,40 @@ local background2
 
 local obstacleTable = {}
 
+local died = false
+local hitobstacle = false
+local lives = 3
+local score = 0
+_G.finalScore = 0
 local livesText
 local scoreText
-local uiGroup
+local backGroup = display.newGroup()
+local obstacleGroup = display.newGroup()
+local uiGroup = display.newGroup()
+local floorGroup = display.newGroup()
+local heroGroup = display.newGroup()
+
+
+local function gotoDead()
+	composer.gotoScene( "scenes.dead", { time=400, effect="crossFade" } )
+end
 
 local function createObstacle()
- 
-	local newObstacle = display.newImageRect("images/rock.png", 155, 100)
-	--newObstacle.strokeWidth = 3
-	--newObstacle:setFillColor( 0.5 )
-	--newObstacle:setStrokeColor( 0, 0, 1 )
-	newObstacle.x = 2050
-	newObstacle.y = 642
-	local rockShape = {  -5,-50, 54,-13, 75,46, -75,46, -64,-10 }
-	physics.addBody( newObstacle, "static", { shape=rockShape } )
-	--newObstacle:setLinearVelocity(-600, 0)
-	transition.to( newObstacle, { time=6000, alpha=1, x=-700, y=newObstacle.y} )
-    table.insert( obstacleTable, newObstacle )
-	newObstacle.myName = "obstacle"
-	newObstacle.isFixedRotation = true
-
+	if(died == false) then
+		local newObstacle = display.newImageRect(obstacleGroup, "images/rock.png", 155, 100)
+		--newObstacle.strokeWidth = 3
+		--newObstacle:setFillColor( 0.5 )
+		--newObstacle:setStrokeColor( 0, 0, 1 )
+		newObstacle.x = 2050
+		newObstacle.y = 642
+		local rockShape = {  -5,-50, 54,-13, 75,46, -75,46, -64,-20, -17,-20, -5,-50 }
+		physics.addBody( newObstacle, "static", { shape=rockShape } )
+		--newObstacle:setLinearVelocity(-600, 0)
+		transition.to( newObstacle, { time=6000, alpha=1, x=-700, y=newObstacle.y, tag="transTag"} )
+		table.insert( obstacleTable, newObstacle )
+		newObstacle.myName = "obstacle"
+		newObstacle.isFixedRotation = true
+	end
 end
 
 local function gameLoop()
@@ -62,32 +76,155 @@ local function gameLoop()
 	-- end
 
 	-- Dispose of obstacles that drifted off screen
-	for i = #obstacleTable, 1, -1 do
-        local thisObstacle = obstacleTable[i]
- 
-        if ( thisObstacle.x < -100 or thisObstacle.y > 770)
-		then
-            display.remove( thisObstacle )
-            table.remove( obstacleTable, i )
-        end
-    end
-
+	if(died == false and hitobstacle == false) then
+		for i = #obstacleTable, 1, -1 do
+			local thisObstacle = obstacleTable[i]
+	
+			if ( thisObstacle.x < -100 or thisObstacle.y > 770) then
+				display.remove( thisObstacle )
+				table.remove( obstacleTable, i )
+			end
+		end
+	end
 end
+
+local function timerScore()
+	score = score + 1
+	scoreText.text = "Score: " .. score 
+end
+
+--Hero stay on position after hit an obstacle
+local function stay()
+	if(died == false) then
+		if(hero.x < 100 or hero.x > 100) then
+			hero.x = 100
+		end
+	end
+end
+
+--check hero LinearVelocity
+local function checkrun()
+	if(died == false) then
+		local x = hero:getLinearVelocity()
+		if(math.floor(hero.y) > 527 and (x < 0 or x > 0)) then
+			hero:setLinearVelocity(0, 0)
+		end
+	end
+end
+
+scoreTimer = timer.performWithDelay(1000, timerScore, 0)
+timer.performWithDelay(500, stay, 0)
+timer.performWithDelay(1000, checkrun, 0)
+
+local function visible()
+	timer.performWithDelay(100, 
+		function() 
+			hero.isVisible = false
+		end )
+	timer.performWithDelay(200, 
+		function() 
+			hero.isVisible = true			
+		end )
+	timer.performWithDelay(300, 
+		function() 
+			hero.isVisible = false
+		end )
+	timer.performWithDelay(400, 
+		function() 
+			hero.isVisible = true
+		end )
+	timer.performWithDelay(500, 
+		function() 
+			hero.isVisible = false
+		end )
+	timer.performWithDelay(600, 
+		function() 
+			hero.isVisible = true
+		end )
+	timer.performWithDelay(700, 
+		function() 
+			hero.isVisible = false
+		end )
+	timer.performWithDelay(800, 
+		function() 
+			hero.isVisible = true
+		end )
+end
+
+local function onCollision( event )
+ 
+	if ( event.phase == "began" ) then
+		 if(hitobstacle == false) then
+
+			local obj1 = event.object1
+			local obj2 = event.object2
+		
+			if ( ( obj1.myName == "obstacle" and obj2.myName == "heroName" ) or ( obj1.myName == "heroName" and obj2.myName == "obstacle" ) )then
+				
+					lives = lives - 1
+					livesText.text = "Lives: " .. lives
+				
+				if(lives ~= 0 ) then
+					if(obj1.myName == "obstacle") then
+						hitobstacle = true
+						obj1.isSensor = true
+						visible()
+					end
+
+					if(obj2.myName == "obstacle") then
+						hitobstacle = true
+						obj2.isSensor = true
+						visible()
+					end
+				end
+				if ( lives == 0 ) then
+					died = true
+					hero.isSensor = true
+					hero:setLinearVelocity( 0, -1000 )
+					
+
+					local loseText = display.newText( uiGroup, "Przegrałeś!", display.contentCenterX, 250, "fonts/Pixellari.ttf", 100 )
+						loseText:setFillColor( 249/255, 111/255, 41/255 )
+
+						Runtime:removeEventListener("collision", onCollision)
+						transition.cancel( "transTag" )
+						timer.cancel(scoreTimer)
+						hero:pause()
+						
+					timer.performWithDelay( 3000, 
+					function() 
+						_G.finalScore = score;
+						gotoDead()
+					end )
+				end
+
+				timer.performWithDelay(500, 
+				function() 
+					hitobstacle = false
+				end )
+			end
+		end
+	end
+	return true
+end
+
+Runtime:addEventListener( "collision", onCollision )
 
 local function onKeyEvent( event )
 	-- Jumping
-	if ( event.keyName == "space" ) then
-		if (event.phase == "down" and math.floor(hero.y) > 525 ) then
-			hero.isJumping = true
-			hero:setSequence( "jumpStart" )
-			hero:play()
+	if(died == false) then 
+		if ( event.keyName == "space" ) then
+			if (event.phase == "down" and math.floor(hero.y) > 525 ) then
+				hero.isJumping = true
+				hero:setSequence( "jumpStart" )
+				hero:play()
 
-			local vx, vy = hero:getLinearVelocity()
-			hero:setLinearVelocity( vx, 0 )
-			hero:applyLinearImpulse( nil, -550, hero.x, hero.y )
-        end
+				local vx, vy = hero:getLinearVelocity()
+				hero:setLinearVelocity( vx, 0 )
+				hero:applyLinearImpulse( nil, -550, hero.x, hero.y )
+			end
+		end
 	end
-
     return false
 end
 
@@ -130,7 +267,7 @@ end
 
 local function scrollBackground(background)
 	background.x = 2870
-	transition.to( background, { time=60000, alpha=1, x=-960, y=background.y, onComplete=scrollBackground} )
+	transition.to( background, { time=60000, alpha=1, x=-960, y=background.y, onComplete=scrollBackground, tag="transTag"} )
 end
 
 local function scrollFloor(floor)
@@ -140,7 +277,7 @@ local function scrollFloor(floor)
 	else
 		floor.x = floor1.x + 1370
 	end
-	transition.to( floor, { time=6000, alpha=1, x=-700, y=floor.y, onComplete=scrollFloor} )
+	transition.to( floor, { time=6000, alpha=1, x=-700, y=floor.y, onComplete=scrollFloor, tag="transTag"} )
 end
 
 
@@ -154,38 +291,50 @@ function scene:create( event )
 	local sceneGroup = self.view
 	-- Code here runs when the scene is first created but has not yet appeared on screen
 	uiGroup = display.newGroup()    -- Display group for UI objects like the score
+	sceneGroup:insert( backGroup )
 	sceneGroup:insert( uiGroup ) 
+	sceneGroup:insert( obstacleGroup )
+	sceneGroup:insert( floorGroup )
+	sceneGroup:insert( heroGroup ) 
 
+	died = false
 	physics.pause()
 
-	background1 = display.newImageRect( "images/background_run.png", 1920, 1080 )
+	livesText = display.newText( uiGroup, "Lives: " .. lives, 100, 60, "fonts/Pixellari.ttf", 50 )
+	scoreText = display.newText( uiGroup, "Score: " .. score, 640, 60, "fonts/Pixellari.ttf", 50 )
+	score = 0
+	lives = 3
+	scoreText.text = "Score: " .. score
+	livesText.text = "Lives: " .. lives 
+
+	background1 = display.newImageRect( backGroup, "images/background_run.png", 1920, 1080 )
 	background1.x = 960
 	background1.y = display.contentCenterY
-	transition.to( background1, { time=30000, alpha=1, x=-960, y=background1.y, onComplete=scrollBackground} )
+	transition.to( background1, { time=30000, alpha=1, x=-960, y=background1.y, onComplete=scrollBackground, tag="transTag"} )
 
-	background2 = display.newImageRect( "images/background_run.png", 1920, 1080 )
+	background2 = display.newImageRect(backGroup,  "images/background_run.png", 1920, 1080 )
 	background2.x = background1.x + 1910
 	background2.y = display.contentCenterY
-	transition.to( background2, { time=60000, alpha=1, x=-960, y=background2.y, onComplete=scrollBackground} )
-
-	floor1 = display.newImageRect("images/platform.png", 1400 , 305)
+	transition.to( background2, { time=60000, alpha=1, x=-960, y=background2.y, onComplete=scrollBackground, tag="transTag"} )
+		
+	floor1 = display.newImageRect(floorGroup, "images/platform.png", 1400 , 305)
 	floor1.x = 700
 	floor1.y = 800
 	floor1.id = "ground"
-
+	
 	local floorShape = { -700,-125, 700,-125, 700,152, -700,152 }
 	physics.addBody( floor1, "static", {shape=floorShape, bounce=0.0 } )
-	transition.to( floor1, { time=3000, alpha=1, x=-700, y=floor1.y,  onComplete=scrollFloor} )
+	transition.to( floor1, { time=3000, alpha=1, x=-700, y=floor1.y,  onComplete=scrollFloor, tag="transTag"} )
 
-	floor2 = display.newImageRect("images/platform.png", 1400 , 305)
+	floor2 = display.newImageRect(floorGroup, "images/platform.png", 1400 , 305)
 	floor2.x = 2070
 	floor2.y = 800
 	floor2.id = "ground"
 	physics.addBody( floor2, "static", { shape=floorShape, bounce=0.0 } )
-	transition.to( floor2, { time=6000, alpha=1, x=-700, y=floor1.y,  onComplete=scrollFloor} )
-
+	transition.to( floor2, { time=6000, alpha=1, x=-700, y=floor1.y,  onComplete=scrollFloor, tag="transTag"} )
+	
 	-- Create hero
-	hero = display.newSprite( heroRunning, heroSequences )
+	hero = display.newSprite(heroGroup, heroRunning, heroSequences )
 	hero.x = 100
 	hero.y = floor1.y - 220
 
@@ -199,6 +348,7 @@ function scene:create( event )
 
 	hero.isFixedRotation = true
 	hero.isJumping = false
+	hero.myName = "heroName"
 
 	hero.collision = sensorCollision
 
@@ -242,6 +392,7 @@ function scene:hide( event )
 
 	elseif ( phase == "did" ) then
 		-- Code here runs immediately after the scene goes entirely off screen
+		composer.removeScene( "scenes.game" )
 	end
 end
 
